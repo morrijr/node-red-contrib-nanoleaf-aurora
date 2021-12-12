50526432a832fba0bda85d8501124f91633b0c57
Andrey Popov <andrey_popov@me.com>
2021-12-12 12:08:01 +0300

    event auto-reconnect

--- nodes/event.js
+++ nodes/event.js
@@ -1,93 +1,104 @@
-module.exports = function (RED) {
-    'use strict'
+const EventSource = require('../lib/eventsource.js');
+module.exports = function(RED) {
+  'use strict';
 
-    const EventSource = require('../lib/eventsource.js')
+  // const Ping = require('ping-url');
+  var ping = require('ping');
+  const EventSource = require('../lib/eventsource.js');
 
-    function Event(config) {
-        RED.nodes.createNode(this, config)
-        var node = this
-        var installationObj = RED.nodes.getNode(config.installation)
+  function Event(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    var installationObj = RED.nodes.getNode(config.installation);
 
-        if (installationObj && installationObj.host) {
-            node.url = `http://${installationObj.host}:${installationObj.port}${installationObj.base}${installationObj.accessToken}`
-            connect();
-        }
+    if (installationObj && installationObj.host) {
+      node.url = `http://${installationObj.host}:${installationObj.port}${installationObj.base}${installationObj.accessToken}`;
+      connect();
 
-        node.status({fill: 'red', shape: 'ring', text: 'disconnected'})
-
-
-        function handleEvent(e, eventType) {
-            if (e.type === 'open')  return
-
-            var payload = e.data;
-            try {
-                payload = JSON.parse(e.data).events[0]
-            } catch (e) {
-                node.warn(payload);
-            }
-
-            node.send({
-                event: eventType,
-                payload: payload,
-                payload_raw: e.data
-            })
-        }
-
-        function connect() {
-            if (!node.client1) {
-
-                if (!node.url) {
-                    node.status({fill: 'red', shape: 'dot', text: 'no url'})
-                    return
-                }
-
-                // Start a new stream (i.e. send a new http get)
-                node.client1 = new EventSource(node.url+'/events?id=1')
-                node.client2 = new EventSource(node.url+'/events?id=2')
-                node.client3 = new EventSource(node.url+'/events?id=3')
-                node.client4 = new EventSource(node.url+'/events?id=4')
-
-                node.client1.onopen = function() {
-                    node.status({fill: 'green', shape: 'dot', text: 'connected'})
-                }
-                node.client1.onerror = function(err) {
-                    node.status({fill: 'red', shape: 'dot', text: `Error: ${err.message}`})
-                }
-            }
-
-            // Handle ALL events.
-            node.client1.onAnyMessage = function(eventType, event) {
-                handleEvent(event, 'state')
-            }
-            node.client2.onAnyMessage = function(eventType, event) {
-                handleEvent(event, 'layout')
-            }
-            node.client3.onAnyMessage = function(eventType, event) {
-                handleEvent(event, 'effects')
-            }
-            node.client4.onAnyMessage = function(eventType, event) {
-                handleEvent(event, 'touch')
-            }
-        }
-
-        node.on('close', function() {
-            node.status({fill: 'red', shape: 'ring', text: 'disconnected'})
-
-            if (node.client1) {
-                node.client1.close()
-            }
-            if (node.client2) {
-                node.client2.close()
-            }
-            if (node.client3) {
-                node.client3.close()
-            }
-            if (node.client4) {
-                node.client4.close()
-            }
-        })
+      //check connection
+      setInterval(function(){
+        ping.sys.probe(installationObj.host, function(isAlive){
+          if (!isAlive) {
+            closeClients();
+          }
+          connect();
+        });
+      },10000);
     }
 
-    RED.nodes.registerType('event', Event)
-}
+    node.status({fill: 'red', shape: 'ring', text: 'disconnected'});
+
+    function handleEvent(e) {
+      if (e.type === 'open') return;
+      if (e.type === 'error') return;
+
+      var payload = e.data;
+      try {
+        payload = JSON.parse(e.data).events[0];
+      } catch (e) {
+        node.warn(payload);
+      }
+
+      let eventType = '';
+      if (e.lastEventId == 1) {
+        eventType = 'state';
+      } else if (e.lastEventId == 2) {
+        eventType = 'layout';
+      } else if (e.lastEventId == 3) {
+        eventType = 'effect';
+      } else if (e.lastEventId == 4) {
+        eventType = 'touch';
+      }
+
+      node.send({
+        event: eventType,
+        event_id: parseInt(e.lastEventId),
+        payload: payload,
+        payload_raw: e.data,
+      });
+    }
+
+    function connect() {
+      if (!node.client) {
+
+        if (!node.url) {
+          node.status({fill: 'red', shape: 'dot', text: 'no url'});
+          return;
+        }
+
+        // Start a new stream (i.e. send a new http get)
+        node.client = new EventSource(node.url + '/events?id=1,2,3,4');
+
+        node.client.onopen = function() {
+          node.status({fill: 'green', shape: 'dot', text: 'connected'});
+          node.log('Connected')
+        };
+        node.client.onerror = function(err) {
+          node.log(err);
+          node.status({fill: 'red', shape: 'dot', text: `Error: ${err.message}`});
+          closeClients();
+        };
+        node.client.onAnyMessage = function(eventType, event) {
+          handleEvent(event);
+        };
+      }
+    }
+
+    node.on('close', function() {
+      closeClients();
+    });
+
+    function closeClients() {
+      node.status({fill: 'red', shape: 'ring', text: 'disconnected'});
+      if (node.client) {
+        node.client.close();
+        node.client = null;
+      }
+    }
+
+
+  }
+
+  RED.nodes.registerType('event', Event);
+};
 
